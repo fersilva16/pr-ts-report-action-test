e0e4ac5d7f6e2f6feb7d87b1c36dbc70140b7e9c
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-19 13:09:49 -0300

    feat(report-script): add comment

--- scripts/report.js
+++ scripts/report.js
@@ -1,5 +1,5 @@
 const { spawnSync } = require('child_process');
-const path = require('path');
+const { Octokit } = require('octokit');
 
 const result = spawnSync('yarn', ['--silent', 'tsc', '--noEmit']);
 
@@ -20,10 +20,40 @@ if (result.status !== 2) {
   process.exit(1);
 }
 
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const owner = 'fersilva16';
+const repo = 'pr-ts-report-action';
+
 const locations = stdout
   .trim()
   .split('\n')
   .filter((line) => !line.match(/^\s+/))
-  .map((line) => line.split(':')[0]);
+  .map((line) => {
+    const [location, ...rest] = line.split(':');
+    const githubPath = location.replace(
+      /\(([0-9]+),[0-9]+\)$/,
+      (_, l) => `#L${l}`
+    );
 
-console.log(locations);
+    return {
+      location,
+      url: `https://github.com/${owner}/${repo}/tree/main/${githubPath}`,
+      error: rest.join(':'),
+    };
+  });
+
+octokit.rest.issues.createComment({
+  owner,
+  repo,
+  issue_number: parseInt(process.env.PR_NUMBER),
+  body: [
+    '| Location | Error |',
+    '| -------- | ----- |',
+    ...locations.map(
+      ({ location, url, error }) => `| [${location}](${url}) | \`${error}\` |`
+    ),
+  ].join('\n'),
+});
