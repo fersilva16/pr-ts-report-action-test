6861bdaf2a62afbe12e2c0b1bc2476e70373a466
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-19 20:27:58 -0300

    test(fun2): add error (#16)

--- src/fun2.ts
+++ src/fun2.ts
@@ -6,6 +6,7 @@ export type Fun2Options = {
 
 export const fun2 = (options: Fun2Options): Fun2Options => {
   if (options.a === true) return options;
+  if (options.a === false) return options;
 
   return {
     a: '',
